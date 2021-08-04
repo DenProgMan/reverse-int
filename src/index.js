08e5ed28a98737c7a5d53a4ecb0a2d569bfd5029
DenProgMan <rkden58@gmail.com>
2021-08-04 16:41:06 +0300

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    const numPositive = Math.abs(n);
+    const numArray = numPositive.toString().split('');
+    numArray.reverse();
+
+    return +numArray.join('');
 }
